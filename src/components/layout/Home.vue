0e463faffe23f6b18d29a52eb469048baca6c3cc
Fabio Carvalho <fabiopbcarvalho@gmail.com>
2024-02-29 18:01:32 -0300

    ci: rerun

--- src/components/layout/Home.vue
+++ src/components/layout/Home.vue
@@ -2,7 +2,7 @@
    <div class="min-w-full min-h-screen bg-blue-300">
       <section>
          <h1 class="text-2xl uppercase text-center text-gray-800">
-            Embarcação from AWS + Github + agora vai
+            Embarcação from AWS + Github + atualizado
          </h1>
       </section>
    </div>
