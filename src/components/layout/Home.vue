581bea7ab92ec6424912de4b359fff0383899d17
Fabio Carvalho <fabiopbcarvalho@gmail.com>
2024-02-29 16:48:58 -0300

    add config para deploy aws

--- src/components/layout/Home.vue
+++ src/components/layout/Home.vue
@@ -2,7 +2,7 @@
    <div class="min-w-full min-h-screen bg-blue-300">
       <section>
          <h1 class="text-2xl uppercase text-center text-gray-800">
-            Embarcação
+            Embarcação from AWS + Github
          </h1>
       </section>
    </div>
